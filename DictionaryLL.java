37490ce6233ca931bc7cf87c9d0dd090163cb3fa
traciklopez <traciklopez@gmail.com>
2016-04-13 14:58:00 -0700

    init

---
+++ DictionaryLL.java
@@ -0,0 +1,57 @@
+package spelling;
+
+import java.util.LinkedList;
+
+/**
+ * A class that implements the Dictionary interface using a LinkedList
+ *public class MyList {
+    private LinkedList<Integer> list;
+
+    public MyList(LinkedList<Integer> list) {
+        this.list = list;
+    }
+ */
+public class DictionaryLL implements Dictionary 
+{
+	private LinkedList<String> dict;
+	
+    // TODO: Add a constructor
+	public DictionaryLL() {
+		dict = new LinkedList<String>();
+		
+		
+	}
+
+
+    /** Add this word to the dictionary.  Convert it to lowercase first
+     * for the assignment requirements.
+     * @param word The word to add
+     * @return true if the word was added to the dictionary 
+     * (it wasn't already there). */
+    public boolean addWord(String word) {
+    	String lower = word.toLowerCase();
+    	if (!(dict.contains(lower))) {
+    		dict.add(lower);
+    	}
+    		return true; 
+    }
+
+
+    /** Return the number of words in the dictionary */
+    public int size()
+    {
+        // TODO: Implement this method
+        return dict.size();
+    }
+
+    /** Is this a word according to this dictionary? */
+    public boolean isWord(String s) {
+       String w = s.toLowerCase();
+       if (dict.contains(w)) {
+    	   return true; 
+       }
+        return false;
+    }
+
+    
+}
